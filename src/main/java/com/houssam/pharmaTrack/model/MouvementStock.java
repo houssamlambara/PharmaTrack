e4a149d07a48a53a81e77ef230527ad1177ffb53
houssamlambara <lambarahoussam@gmail.com>
2025-12-25 09:44:46 +0100

    Ajout du modèle MouvementStock

---
+++ src/main/java/com/houssam/pharmaTrack/model/MouvementStock.java
@@ -0,0 +1,42 @@
+package com.houssam.pharmaTrack.model;
+
+import com.houssam.pharmaTrack.enums.MovementType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "mouvement_stock")
+public class MouvementStock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private MovementType type;
+
+    @Column(nullable = false)
+    private Integer quantite;
+
+    @Column(nullable = false)
+    private LocalDateTime dateHeure = LocalDateTime.now();
+
+    @Column(length = 100)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medicamenr_id", nullable = false)
+    private Medicament medicament;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
