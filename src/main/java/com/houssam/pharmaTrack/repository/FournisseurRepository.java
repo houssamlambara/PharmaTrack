e72724f5af3944203867da02e69fa036ab8d7865
houssamlambara <lambarahoussam@gmail.com>
2026-01-15 14:17:45 +0100

    update repository

--- src/main/java/com/houssam/pharmaTrack/repository/FournisseurRepository.java
+++ src/main/java/com/houssam/pharmaTrack/repository/FournisseurRepository.java
@@ -1,4 +1,24 @@
 package com.houssam.pharmaTrack.repository;
 
-public interface FournisseurRepository {
+import com.houssam.pharmaTrack.model.Fournisseur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FournisseurRepository extends JpaRepository<Fournisseur, String> {
+
+    Optional<Fournisseur> findByEmail(String email);
+
+    Optional<Fournisseur> findByTelephone(String telephone);
+
+    List<Fournisseur> findByNomContainingIgnoreCase(String nom);
+
+    List<Fournisseur> findByActif(Boolean actif);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByTelephone(String telephone);
 }
