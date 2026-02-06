ee995e184056fbb829e1920e2c8ef6241bd2106e
houssam <lambarahoussam@gmail.com>
2026-02-06 10:59:46 +0100

    add commandeitems logique mitier

--- src/main/java/com/houssam/pharmaTrack/service/CommandeItemsService.java
+++ src/main/java/com/houssam/pharmaTrack/service/CommandeItemsService.java
@@ -1,4 +1,14 @@
 package com.houssam.pharmaTrack.service;
 
+import com.houssam.pharmaTrack.dto.responseDTO.CommandeItemsResponseDTO;
+
+import java.util.List;
+
 public interface CommandeItemsService {
+
+    List<CommandeItemsResponseDTO> getByCommande(String commandeId);
+
+    List<CommandeItemsResponseDTO> getByMedicament(String medicamentId);
+
+    CommandeItemsResponseDTO updateQuantiteRecue(String id, Integer quantiteRecue);
 }
