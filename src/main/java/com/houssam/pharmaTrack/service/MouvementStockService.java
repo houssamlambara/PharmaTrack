19414990dbf8d5f9ee0893db2eba9de90533adf9
houssam <lambarahoussam@gmail.com>
2026-02-09 09:12:02 +0100

    ajouter mouvemetn service avec la logique metier necessaire pour suivre les mouvement

--- src/main/java/com/houssam/pharmaTrack/service/MouvementStockService.java
+++ src/main/java/com/houssam/pharmaTrack/service/MouvementStockService.java
@@ -1,4 +1,31 @@
 package com.houssam.pharmaTrack.service;
 
+import com.houssam.pharmaTrack.dto.requestDTO.MouvementStockRequestDTO;
+import com.houssam.pharmaTrack.dto.responseDTO.MouvementStockResponseDTO;
+import com.houssam.pharmaTrack.enums.MovementType;
+import com.houssam.pharmaTrack.model.Medicament;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface MouvementStockService {
+
+    MouvementStockResponseDTO create(MouvementStockRequestDTO requestDTO);
+
+    void enregistrerEntree(Medicament medicament, Integer quantite, String reference);
+
+    void enregistrerSortie(Medicament medicament, Integer quantite, String reference);
+
+    List<MouvementStockResponseDTO> getAll();
+
+    MouvementStockResponseDTO getById(String id);
+
+    List<MouvementStockResponseDTO> getByMedicament(String medicamentId);
+
+    List<MouvementStockResponseDTO> getByType(MovementType type);
+
+    List<MouvementStockResponseDTO> getByDateRange(LocalDateTime dateDebut, LocalDateTime dateFin);
+
+    List<MouvementStockResponseDTO> getByUser(String userId);
 }
