94cabd7bac65d986a6dc9db3cc99603493033fa1
houssam <lambarahoussam@gmail.com>
2026-03-10 09:50:55 +0000

    mise a jour des routes medicament et nettoyage ui

--- FrontEnd/src/app/features/medicament/medicament.routes.ts
+++ FrontEnd/src/app/features/medicament/medicament.routes.ts
@@ -3,7 +3,6 @@ import { ListMedicamentComponent } from './list-medicament/list-medicament';
 import { AddMedicamentComponent } from './add-medicament/add-medicament';
 
 export const medicamentRoutes: Routes = [
-  { path: '', component: ListMedicamentComponent },
-  { path: 'add', component: AddMedicamentComponent }
+  { path: '', component: ListMedicamentComponent }
 ];
 
